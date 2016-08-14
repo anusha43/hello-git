9367f74c2603f24f8c741e3efc5698a78461188b
anusha43 <puvadaanusha4@gmail.com>
2016-08-13 21:59:53 -0500

    HelloGit changed

---
+++ HelloGit/src/com/project/HelloGitClass.java
@@ -0,0 +1,14 @@
+package com.project;
+
+public class HelloGitClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello git Program executed successfully");
+
+	}
+
+}
